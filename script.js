d5716af73c1b33567397e59db4f79d930c6ef761
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-02 22:07:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function mapLetters(string) {
+	const map = {};
+	for (let i = 0; i < string; i++) {
+		const c = string[i];
+		if (map[c]) {
+			map[c].push(i);
+		} else {
+			map[c] = [i];
+		}
+	}
+	return map;
+}
